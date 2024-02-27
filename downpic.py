f96cd88a03d03acb5f9a3aa32aeaae087c9bc8e0
Diego Silva <diwalker13@icloud.com>
2024-02-26 21:19:00 -0300

    user-agent

--- downpic.py
+++ downpic.py
@@ -10,7 +10,7 @@ import re
 
 async def baixar_imagem(sem, session, url, destino):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36',
+        'User-Agent': 'User agent aqui'
         'Cookie': 'Cookie aqui'
     }
 
